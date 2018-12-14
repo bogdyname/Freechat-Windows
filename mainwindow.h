d012e0e069a13b6400aa0824222a5bfc68b463a9
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-14 18:17:45 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,56 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QScrollArea>
+#include <QLabel>
+#include <QApplication>
+
+class ImageViewer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageViewer();
+
+private slots:
+    void open();
+    void print();
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
+    void fitToWindow();
+    void about();
+
+private:
+    void createActions();
+    void createMenus();
+    void updateActions();
+    void scaleImage(double factor);
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    double scaleFactor;
+
+#ifndef QT_NO_PRINTER
+    QPrinter printer;
+#endif
+
+    QAction *openAct;
+    QAction *printAct;
+    QAction *exitAct;
+    QAction *zoomInAct;
+    QAction *zoomOutAct;
+    QAction *normalSizeAct;
+    QAction *fitToWindowAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+
+    QMenu *fileMenu;
+    QMenu *viewMenu;
+    QMenu *helpMenu;
+};
 
 namespace Ui {
 class MainWindow;
