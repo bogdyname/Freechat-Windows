e83fa79a1fb21a734d9c160cb08e029377d3246e
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-14 18:27:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ using namespace Qt;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
