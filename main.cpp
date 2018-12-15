501b29169ea06ec17aa4f45d75f22d42839b25fb
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-15 22:59:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QWidget>
 
 using namespace Qt;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow w;
-    w.show();
+    QWidget wgt;
+        QPixmap pix("/freechat.jpg");
+        QCursor cur(pix);
+
+        wgt.setCursor(cur);
+        wgt.resize(25, 25);
+        wgt.show();
 
     return app.exec();
 }
