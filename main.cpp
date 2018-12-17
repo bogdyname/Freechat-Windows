b550cbc84abc6e4c391c3c8a85ae1005d682c756
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-17 18:44:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QWidget>
+#include <QFont>
+#include <QPushButton>
 
 using namespace Qt;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QWidget wgt;
+    QWidget Freechat;
     QPixmap pix("D:/Freechat/Freechat/Freechat/unnamed.png");
     QCursor cur(pix);
 
-    wgt.setCursor(cur);
-    wgt.resize(750, 550);
-    wgt.show();
+    QPushButton quit("Quit", &Freechat);
+    quit.setFont(QFont("Terminal", 18, QFont::Bold));
+    quit.setGeometry(10, 40, 180, 40);
+    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
+
+    Freechat.setCursor(cur);
+    Freechat.resize(750, 550);
+    Freechat.show();
 
     return app.exec();
 }
