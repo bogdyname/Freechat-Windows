72bfd974162d4bf3c782bddda56baf7b9660f189
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-17 11:57:59 -0500

    Add files via upload

---
+++ imageviewer.cpp
@@ -0,0 +1,20 @@
+#include "imageviewer.h"
+
+ImageViewer::ImageViewer()
+{
+         imageLabel = new QLabel;
+         imageLabel->setBackgroundRole(QPalette::Base);
+         imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+         imageLabel->setScaledContents(true);
+
+         scrollArea = new QScrollArea;
+         scrollArea->setBackgroundRole(QPalette::Dark);
+         scrollArea->setWidget(imageLabel);
+         setCentralWidget(scrollArea);
+
+         createActions();
+         createMenus();
+
+         setWindowTitle(tr("Image Viewer"));
+         resize(500, 400);
+}
