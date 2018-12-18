2d68d8edda6c5e4dcd38bd0b2d655b2b9ccb7c80
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-18 23:19:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
+#include "imageviewer.h"
 #include <QApplication>
 #include <QWidget>
 #include <QFont>
 #include <QPushButton>
-#include "imageviewer.h"
 
 using namespace Qt;
 
@@ -11,20 +11,23 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QWidget Freechat;
+    MainWindow w;
+    w.show();
+
     QPixmap pix("D:/Freechat/Freechat/Freechat/unnamed.png");
     QCursor cur(pix);
 
-    QPushButton quit("Quit", &Freechat);
+    QPushButton quit("Quit", &w);
     quit.setFont(QFont("Segoe Print", 12, QFont::Bold));
     quit.setGeometry(700, 525, 50, 25);
     QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
 
-    QPushButton call("Call request", &Freechat);
+    QPushButton call("Call request", &w);
     call.setFont(QFont("Segoe Print", 12, QFont::Bold));
     call.setGeometry(640, 0, 110, 30);
     QObject::connect(&call, SIGNAL(clicked()), &app, SLOT(call));
 
-    QPushButton friendList("Show friend list", &Freechat);
+    QPushButton friendList("Show friend list", &w);
     friendList.setFont(QFont("Segoe Print", 12, QFont::Bold));
     friendList.setGeometry(0, 0, 140, 35);
     QObject::connect(&friendList, SIGNAL(clicked()), &app, SLOT(call));
@@ -32,9 +35,9 @@ int main(int argc, char *argv[])
     //ImageViewer imageViewer;
     //imageViewer.show();
 
-    Freechat.setCursor(cur);
-    Freechat.resize(750, 550);
-    Freechat.show();
+    w.setCursor(cur);
+    w.resize(750, 550);
+    //Freechat.show();
 
     return app.exec();
 }
