11c9d7a92911ddc0ed934d24a33cc8c0ac120fc1
bogdyname <43905628+bogdyname@users.noreply.github.com>
2018-12-22 22:48:46 +0300

    Update networkcode.h

--- networkcode.h
+++ networkcode.h
@@ -1,11 +1,25 @@
 #ifndef NETWORKCODE_H
 #define NETWORKCODE_H
-
+#include <QDialog>
+#include <QTcpSocket>
 
 class networkcode
 {
 public:
     networkcode();
+};
+
+class Dialog : public QDialog
+{
+private slots:
+    void onSokConnected();
+    void onSokDisconnected();
+    void onSokReadyRead();
+    void onSokDisplayError(QAbstractSocket::SocketError socketError);
+private:
+    QTcpSocket *_sok;
+    quint16 _blockSize;
+    QString _name;
 };
 
 #endif // NETWORKCODE_H
