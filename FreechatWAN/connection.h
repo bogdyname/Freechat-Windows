cd75ba3e251c04f3c158e66794c1ed8f2c4a3361
Bogdyname <bogdyname@gmail.com>
2019-02-25 21:25:20 +0300

    update connection.h
    -- added new connection for local host PC (maybe for second user, I do not know)

--- FreechatWAN/connection.h
+++ FreechatWAN/connection.h
@@ -7,7 +7,40 @@
 #define CONNECTION_H
 
 #include <QtNetwork>
+#include <QHostAddress>
+#include <QTcpSocket>
 #include <QSctpSocket>
+
+class Connection : public QSctpSocket
+{
+    Q_OBJECT
+
+public:
+    enum ConnectionData
+    {
+        WaintinForConnection,
+        WritingForConnection,
+        ReadyForConnection
+    };
+
+    enum MetaData
+    {
+        Ping,
+        AlreadyConnection,
+        NameOfUser,
+    };
+
+    QSctpSocket *socket = new QSctpSocket(this);
+
+    socket->setMaxChannelCount(1);
+    socket->connectionToHost(QHostAddress::LocalHost, 4433);
+
+    if(socket->waitForConnection(1000))
+    {
+        int inputChannels = socket->readChannelCout();
+        int outputChannels = socket->writeChannelCout();
+    }
+}
 
 
 #endif // CONNECTION_H
