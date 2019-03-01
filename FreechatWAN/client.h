f8e04cc426ac28c1b8d897b2c61ac6408e34ecff
Bogdyname <bogdyname@gmail.com>
2019-03-01 20:10:47 +0300

    update client.h

--- FreechatWAN/client.h
+++ FreechatWAN/client.h
@@ -6,9 +6,27 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-class client : public QTcpSocket
+#include "server.h"
+#include <QAbstractSocket>
+#include <QHostAddress>
+#include <QHash>
 
+class Client : public QObject
 {
+    Q_OBJECT
+
+public:
+    Client();
+
+    void sendMessage(const QString &message);
+    QString nickName() const;
+    bool hasConnection(const QHostAddress &senderIp, int senderPort = -1) const;
+
+signals:
+    void newMessage(const QString &from, const QString &message);
+    void newParticipant(const QString &nick);
+    void participantLeft(const QString &nick);
+
 };
 
 #endif // CLIENT_H
