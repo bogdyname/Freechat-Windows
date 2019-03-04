09fb486d0a7ad532ad606b5ebdc222ce7b4eabcf
Bogdyname <bogdyname@gmail.com>
2019-03-04 21:23:41 +0300

    update4 lol
    maybe a'll get dtank againt

--- Freechat-Windows/Freechat/userclient.h
+++ Freechat-Windows/Freechat/userclient.h
@@ -6,11 +6,40 @@
 #ifndef USERCLIENT_H
 #define USERCLIENT_H
 
+#include "userserver.h"
+#include <QAbstractSocket>
+#include <QHostAddress>
+#include <QHash>
 
-class userclient
+class PeerManager;
+
+class Client : public QObject
 {
-public:
-    userclient();
-};
+    Q_OBJECT
 
+public:
+    Client();
+
+    void sendMessage(const QString &message);
+    QString nickName() const;
+    bool hasConnection(const QHostAddress &senderIp, int senderPort = -1) const;
+
+signals:
+    void newMessage(const QString &from, const QString &message);
+    void newParticipant(const QString &nick);
+    void participantLeft(const QString &nick);
+
+private slots:
+    void newConnection(Connection *connection);
+    void connectionError(QAbstractSocket::SocketError socketError);
+    void disconnected();
+    void readyForUse();
+
+private:
+    void removeConnection(Connection *connection);
+
+    PeerManager *peerManager;
+    Server server;
+    QMultiHash<QHostAddress, Connection *> peers;
+};
 #endif // USERCLIENT_H
