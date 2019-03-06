4d697ae5626e0a57202a54f1ca0f67a5a71890e8
Bogdyname <bogdyname@gmail.com>
2019-03-06 20:37:09 +0300

    update

--- Freechat-Windows/Freechat/userclient.cpp
+++ Freechat-Windows/Freechat/userclient.cpp
@@ -3,9 +3,35 @@
 ***Contact: bogdyname@gmail.com
 */
 
+#include <QtNetwork>
 #include "userclient.h"
+#inclide "connection.h"
 
-userclient::userclient()
+Userclient::Userclient()
 {
+    peerManager = new PeerManager(this);
+    peerManager->setServerPort(server.serverPort());
+    peerManager->startBroadcasting();
 
+    QObject::connect(peerManager, SIGNAL(newConnection(Connection*)),
+                     this, SLOT(newConnection(Connection*)));
+    QObject::connect(&server, SIGNAL(newConnection(Connection*)),
+                     this, SLOT(newConnection(Connection*)));
 }
+
+void Client::sendMessage(const QString &message)
+{
+    if (message.isEmpty())
+        return;
+
+    QList<Connection *> connections = peers.values();
+    foreach (Connection *connection, connections)
+        connection->sendMessage(message);
+}
+
+QString Client::nickName() const
+{
+    return peerManager->userName() + '@' + QHostInfo::localHostName()
+           + ':' + QString::number(server.serverPort());
+}
+
