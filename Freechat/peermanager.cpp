9fecef2b8c26c08561c35ee90a07bc4acac01dcb
Bogdyname <bogdyname@gmail.com>
2019-03-20 13:30:27 +0300

    update peermanager.h
    -- added fun for connection
    -- fun for connection via WLAN (not done)

--- Freechat/peermanager.cpp
+++ Freechat/peermanager.cpp
@@ -1,6 +1,11 @@
+/*
+***Copyright (C) 2019 Freechat Inc
+***Contact: bogdyname@gmail.com
+*/
+
 #include "peermanager.h"
 
-peermanager::peermanager()
+PeerManager::PeerManager()
 {
     /*
      CODE
--- Freechat/peermanager.h
+++ Freechat/peermanager.h
@@ -1,11 +1,49 @@
+/*
+***Copyright (C) 2019 Freechat Inc
+***Contact: bogdyname@gmail.com
+*/
+
 #ifndef PEERMANAGER_H
 #define PEERMANAGER_H
 
+#include <QList>
+#include <QTimer>
+#include <QObject>
+#include <QByteArray>
+#include <QUdpSocket>
 
-class peermanager
+class Client;
+class Connection;
+
+class PeerManager : public QObject
 {
+    Q_OBJECT
+
 public:
-    peermanager();
+    PeerManager(Client *client);
+
+    QString userName() const;
+    void startBroadcasting();
+    void setServerPort(int port);
+    //bool isLocalHostAddress(const QHostAddress &address); Need to connection via WLAN (not only LAN)
+
+signals:
+    void newConnection(Connection *connection);
+
+private slots:
+    void sendBroadcastDatagram();
+    void readBroadcastDatagram();
+
+private:
+    void updateAddresses();
+
+    Client *client;
+    QList<QHostAddress> broadcastAddresses;
+    QList<QHostAddress> ipAddresses;
+    QUdpSocket broadcastSocket;
+    QTimer broadcastTimer;
+    QString username;
+    int serverPort;
 };
 
 #endif // PEERMANAGER_H
