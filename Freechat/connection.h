fd40ab65796fa949a01eccb4a926f1330958e277
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-16 19:14:49 +0300

    refactoring

--- Freechat/connection.h
+++ Freechat/connection.h
@@ -16,71 +16,10 @@
 #include <QString>
 #include <QTimer>
 
-static const int PingInterval = 5 * 1000;
-static const int MaxBufferSize = 1024000;
-static const int PongTimeout = 60 * 1000;
-static const int TransferTimeout = 30 * 1000;
-
 class Connection : public QTcpSocket
 {
-    Q_OBJECT
-
-public:
-    enum ConnectionData
-    {
-        WaintinForConnection,
-        WritingForConnection,
-        ReadyForConnection
-    };
-
-    enum MetaData
-    {
-        AlreadyConnection,
-        Undefined,
-        PlainText,
-        Ping,
-        Pong
-    };
-
-    Connection(QObject *parent = nullptr);
-    Connection(qintptr socketDescriptor, QObject *parent = nullptr);
-    ~Connection();
-
-    QString ReturnVariableUsername() const;
-    void TestMessage(const QString &message);
-    bool SendMessage(const QString &message);
-
-signals:
-    void ReadyForUse();
-    void NewMessage(const QString &from, const QString &message);
-
-protected:
-    void TimerEvent(QTimerEvent *timerEvent);
-    void TimerIdForTimerEvent();
-
-private slots:
-    void ReadyToRead();
-    void PingStatus();
-    void CheckConnection();
-
-private:
-    bool HasEnoughData();
-    void ProcessCheckConnection();
-    void ProcessData();
-
-     QCborStreamReader reader;
-     QCborStreamWriter writer;
-     QString checkingConnection;
-     QString username;
-     QTimer pingTimer;
-     QElapsedTimer pongTime;
-     QString buffer;
-     ConnectionData state;
-     MetaData currentDataType;
-     int transferTimerId;
-     bool isCheckConnection;
+    
 
 };
 
 #endif // CONNECTION_H
-
