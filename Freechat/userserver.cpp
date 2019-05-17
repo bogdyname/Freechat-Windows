ab20654a407b5a6effb6c66ee923e02bb3d84767
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-17 13:59:41 +0300

    refactoring

--- Freechat/userserver.cpp
+++ Freechat/userserver.cpp
@@ -6,15 +6,3 @@
 #include <QtNetwork>
 #include "userserver.h"
 #include "connection.h"
-
-UserServer::UserServer(QObject *parent)
-    : QTcpServer(parent)
-{
-    listen(QHostAddress::Any);
-}
-
-void UserServer::InComingConnection(qintptr socketDescription)
-{
-    Connection *connection = new Connection(socketDescription, this);
-    emit NewConnection(connection);
-}
