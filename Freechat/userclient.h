53283fe464f25f08c5d47562a1e1ae1e97cbde91
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-17 13:59:23 +0300

    refactoring

--- Freechat/userclient.h
+++ Freechat/userclient.h
@@ -15,31 +15,6 @@ class PeerManager;
 
 class UserClient : public QObject
 {
-    Q_OBJECT
-
-public:
-    UserClient();
-
-    QString NickName() const;
-    void SendMessage(const QString &message);
-    bool HasConnection(const QHostAddress &senderIp, int senderPort = -1) const;
-
-signals:
-    void NewParticipant(const QString &nick);
-    void ParticipantLeft(const QString &nick);
-    void NewMessage(const QString &from, const QString &message);
-
-private slots:
-    void ReadyForUse();
-    void Disconnected();
-    void NewConnection(Connection *connection);
-    void ConnectionError(QAbstractSocket::SocketError socketError);
-
-private:
-    void RemoveConnection(Connection *connection);
-
-    UserServer userserver;
-    PeerManager *peerManager;
-    QMultiHash<QHostAddress, Connection *> peers;
+  
 };
 #endif // USERCLIENT_H
