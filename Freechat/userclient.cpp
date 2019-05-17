6de8408d3092f720301b66f0617dfa2f81bcd1ba
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-17 13:59:02 +0300

    refactoring

--- Freechat/userclient.cpp
+++ Freechat/userclient.cpp
@@ -7,37 +7,3 @@
 #include "userclient.h"
 #include "connection.h"
 #include "peermanager.h"
-
-UserClient::UserClient()
-{
-    peerManager = new PeerManager(this);
-    peerManager->SetServerPort(userserver.serverPort());
-    peerManager->StartBroadcasting();
-
-    QObject::connect(peerManager, SIGNAL(NewConnection(Connection*)),
-                     this, SLOT(NewConnection(Connection*)));
-    QObject::connect(&userserver, SIGNAL(NewConnection(Connection*)),
-                     this, SLOT(NewConnection(Connection*)));
-}
-
-void UserClient::SendMessage(const QString &message)
-{
-    if (message.isEmpty())
-    {
-        return;
-    }
-    else
-    {
-        /*clear code*/
-    }
-
-    QList<Connection *> connections = peers.values();
-    foreach (Connection *connection, connections)
-        connection->SendMessage(message);
-}
-
-QString UserClient::NickName() const
-{
-    return peerManager->UserName() + '@' + QHostInfo::localHostName()
-           + ':' + QString::number(userserver.serverPort());
-}
