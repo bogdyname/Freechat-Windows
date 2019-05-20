8e6a46d321628bcbb9a91beb062fb9d19770e7c5
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-20 13:53:40 +0300

    refactoring

--- Freechat/peermanager.h
+++ Freechat/peermanager.h
@@ -12,42 +12,9 @@
 #include <QByteArray>
 #include <QUdpSocket>
 
-class UserClient;
-class Connection;
-
-static const qint32 BroadcastInterval = 2000;
-static const unsigned broadcastPort = 45000;
-
 class PeerManager : public QObject
 {
-    Q_OBJECT
 
-public:
-    PeerManager(UserClient *userclient);
-
-    QString UserName() const;
-    void StartBroadcasting();
-    void SetServerPort(int port);
-    bool IsWLANHostAddress(const QHostAddress &address);
-
-signals:
-    void NewConnection(Connection *connection);
-
-private slots:
-    void SendBroadcastDatagram();
-    void ReadBroadcastDatagram();
-
-private:
-    void UpdateAddresses();
-
-    UserClient *userclient;
-
-    int serverPort;
-    QString username;
-    QTimer broadcastTimer;
-    QUdpSocket broadcastSocket;
-    QList<QHostAddress> ipAddresses;
-    QList<QHostAddress> broadcastAddresses;
 };
 
 #endif // PEERMANAGER_H
