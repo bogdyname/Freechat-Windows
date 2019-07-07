7fb0280cb119196760c19468e3479d0dbb8751e4
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-07-07 23:44:34 +0300

    Update connectionf2f.cpp

--- Freechat/Network/connectionf2f.cpp
+++ Freechat/Network/connectionf2f.cpp
@@ -4,85 +4,23 @@
 */
 
 #include "Network/connectionf2f.h"
+#include "Bin/freechat.h"
 #include "Bin/bin.h"
 
 ConnectionF2F::ConnectionF2F(QObject *parent)
     : QTcpSocket(parent)
 {
-
+    Peerout connecting;
 }
 
-/////////////////////////////////////////////////////
-
-Peerout::Peerout()
+void ConnectionF2F::PassOnWANIp(QString &buffer)
 {
-    connect(socket, SIGNAL(Connected()), this, SLOT(Connected()));
-    connect(socket, SIGNAL(DoConnect()), this, SLOT(DoConnect()));
-    connect(socket, SIGNAL(ReadyRead()), this, SLOT(ReadyRead()));
-    connect(socket, SIGNAL(BytesWrittenOfData(qint64)), this, SLOT(BytesWrittenOfData(qint64)));
-}
-
-Peerout::~Peerout()
-{
-    delete socket;
-}
-
-void Peerout::DoConnect()
-{
-    GetIpAddressFromWAN(strWANip);
-    socket = new QTcpSocket(this);
-
-    socket->connectToHost(strWANip, 80);
-
-        if(socket->waitForConnected(3000))
-        {
-            qDebug() << "Connected!";
-
-            socket->write("DATA OF TEXT");
-            socket->waitForBytesWritten(1000);
-            socket->waitForReadyRead(3000);
-            qDebug() << "Reading: " << socket->bytesAvailable();
-
-            qDebug() << socket->readAll();
-
-            socket->close();
-        }
-        else
-        {
-            qDebug() << "Not connected!";
-        }
-
-        return;
-}
-
-void Peerout::BytesWrittenOfData(qint64 bytes)
-{
-    qDebug() << bytes << " bytes written...";
+    GetIpAddressFromWAN(buffer);
 
     return;
 }
 
-void Peerout::Connected()
-{
-
-    return;
-}
-
-void Peerout::Disconnected()
-{
-
-    return;
-}
-
-void Peerout::ReadyRead()
-{
-    qDebug() << "reading...";
-    qDebug() << socket->readAll();
-
-    return;
-}
-
-void Peerout::GetIpAddressFromWAN(QString &textWithIPAddres)
+void ConnectionF2F::GetIpAddressFromWAN(QString &textWithIPAddres)
 {
         QNetworkAccessManager networkManager;
         QHostAddress IP;
@@ -108,13 +46,9 @@ void Peerout::GetIpAddressFromWAN(QString &textWithIPAddres)
                 IP = ip;
             }
             reply->deleteLater();
-        }
-        );
+        });
 
         textWithIPAddres = IP.toString();
 
         return;
 }
-
-
-/////////////////////////////////////////////////////////////////
